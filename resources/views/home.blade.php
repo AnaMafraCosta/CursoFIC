55ae2cda396d55c4d4f31b085955261af76427a4
Ana Mafra <analuizamafracosta@gmail.com>
2021-09-20 08:33:48 -0300

    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pratica</title>
+</head>
+<body>
+    <h1>Praticando</h1>
+    <h3>Rota extra utilizando um outro template</h3>
+    <a href="{{ route('início')}}">Início</a>
+</body>
+</html>
